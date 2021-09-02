f58d7496c6b6e5c240299aaee4b589fa19be5e34
sia162 <nowitissiya@gmail.com>
2021-09-02 16:19:18 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+export default class App extends Component {
+  pageSize = 15;
+  apikey = process.env.REACT_APP_NEWS_API;
+
+  state = {
+    progress: 0,
+  };
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <LoadingBar color="#f11946" progress={this.state.progress} />
+
+          <Switch>
+            <Route exact path="/">
+              <News
+                setProgress={this.setProgress}
+                apikey={this.apikey}
+                key="general"
+                pageSize={this.pageSize}
+                country="in"
+                category="general"
+                batchColor="danger"
+              />
+            </Route>
+            <Route exact path="/business">
+              <News
+                setProgress={this.setProgress}
+                apikey={this.apikey}
+                key="business"
+                pageSize={this.pageSize}
+                country="in"
+                category="business"
+                batchColor="warning"
+              />
+            </Route>
+            <Route exact path="/entertainment">
+              <News
+                setProgress={this.setProgress}
+                apikey={this.apikey}
+                key="entertainment"
+                pageSize={this.pageSize}
+                country="in"
+                category="entertainment"
+                batchColor="secondary"
+              />
+            </Route>
+            <Route exact path="/general">
+              <News
+                setProgress={this.setProgress}
+                apikey={this.apikey}
+                key="general"
+                pageSize={this.pageSize}
+                country="in"
+                category="general"
+                batchColor="danger"
+              />
+            </Route>
+            <Route exact path="/health">
+              <News
+                setProgress={this.setProgress}
+                apikey={this.apikey}
+                key="health"
+                pageSize={this.pageSize}
+                country="in"
+                category="health"
+                batchColor="success"
+              />
+            </Route>
+            <Route exact path="/science">
+              <News
+                setProgress={this.setProgress}
+                apikey={this.apikey}
+                key="science"
+                pageSize={this.pageSize}
+                country="in"
+                category="science"
+                batchColor="info"
+              />
+            </Route>
+            <Route exact path="/sports">
+              <News
+                setProgress={this.setProgress}
+                apikey={this.apikey}
+                key="sports"
+                pageSize={this.pageSize}
+                country="in"
+                category="sports"
+                batchColor="primary"
+              />
+            </Route>
+            <Route exact path="/technology">
+              <News
+                setProgress={this.setProgress}
+                apikey={this.apikey}
+                key="technology"
+                pageSize={this.pageSize}
+                country="in"
+                category="technology"
+                batchColor="dark"
+              />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let {
+      title,
+      description,
+      imgUrl,
+      newsUrl,
+      author,
+      date,
+      source,
+      batchColor,
+    } = this.props;
+
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              imgUrl
+                ? imgUrl
+                : "https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80"
+            }
+            className="card-img-top"
+            alt="..."
+            style={{ height: "20rem", width: "auto" }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}{" "}
+              <span
+                className={`position-absolute top-0 start-100 translate-middle badge rounded-pill bg-${batchColor}`}
+              >
+                {source}
+              </span>
+            </h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-dark btn-sm"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
